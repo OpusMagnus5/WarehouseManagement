e1832b611a028dd03bd34b6e60cfffbfcde35291
OpusMagnus5 <damianbodzioch95@gmail.com>
2022-07-06 18:55:13 +0200

    new

---
+++ src/main/java/com/damian/bodzioch/warehouse/management/configuration/AppConfiguration.java
@@ -0,0 +1,9 @@
+package com.damian.bodzioch.warehouse.management.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.damian.bodzioch.warehouse.management")
+public class AppConfiguration {
+}
---
+++ src/main/java/com/damian/bodzioch/warehouse/management/controllers/BasicController.java
@@ -0,0 +1,14 @@
+package com.damian.bodzioch.warehouse.management.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BasicController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String main(){
+        return "main.html";
+    }
+}
