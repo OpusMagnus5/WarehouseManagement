1f3004fac05da45f466794e7e3d20d1acb9cc9e5
OpusMagnus5 <damianbodzioch95@gmail.com>
2022-07-11 12:38:55 +0200

    update

--- src/main/java/com/damian/bodzioch/warehouse/management/controllers/LoginController.java
+++ src/main/java/com/damian/bodzioch/warehouse/management/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.damian.bodzioch.warehouse.management.controllers;
 import com.damian.bodzioch.warehouse.management.exceptions.LoginOrPasswordDoNotMatch;
 import com.damian.bodzioch.warehouse.management.model.User;
 import com.damian.bodzioch.warehouse.management.services.iplm.LoginService;
+import com.damian.bodzioch.warehouse.management.session.SessionObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
+
 @Controller
 public class LoginController {
     @Autowired
     LoginService loginService;
 
+    @Resource
+    SessionObject sessionObject;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String logIn(Model model){
         model.addAttribute("user", new User());
+        model.addAttribute("sessionObject", this.sessionObject);
         return "log-in.html";
     }
 
