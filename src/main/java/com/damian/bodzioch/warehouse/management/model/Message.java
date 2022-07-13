2b1d7c22c368d5ddc90231a45e46dadb96369f3b
OpusMagnus5 <damianbodzioch95@gmail.com>
2022-07-13 12:41:13 +0200

    update

---
+++ src/main/java/com/damian/bodzioch/warehouse/management/model/Message.java
@@ -0,0 +1,40 @@
+package com.damian.bodzioch.warehouse.management.model;
+
+public class Message {
+    private String firstName;
+    private String lastName;
+    private String topic;
+    private String content;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
